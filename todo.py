70c3faab99929d2ce9e408d725564e664a4f7597
BD103 <59022059+BD103@users.noreply.github.com>
2022-06-06 07:38:22 -0400

    feat: use with statements to decrease line usage

--- todo.py
+++ todo.py
@@ -1,27 +1,24 @@
 def deleteline(something):
-  f = open('todo.txt')
-  output = []
-  for line in f:
-    if something != line.strip():
-      output.append(line)
-  f.close()
-  f = open('todo.txt', 'w')
-  f.writelines(output)
-  f.close()
+  with open("todo.txt", "rt") as f:
+    output = []
+    for line in f:
+      if something != line.strip():
+        output.append(line)
+  with open("todo.txt", "wt") as f:
+    f.writelines(output)
 try:
   f = open("todo.txt", "x")
 except:
   pass
 while True:
-  f = open("todo.txt", "r")
-  print(f.read())
-  print('\n1.create\n2.delete')
-  e=input('> ')
-  if e=='1':
-    f=open("todo.txt", "a")
-    ee=input('content> ')
-    f.write(f"\n{ee}")
-    f.close()
-  elif e=='2':
-    eee=input('which todo> ')
-    deleteline(eee)
+  with open("todo.txt", "rt") as f:
+    print(f.read())
+    print('\n1.create\n2.delete')
+    e=input('> ')
+    if e=='1':
+      with open("todo.txt", "at") as f:
+        ee=input('content> ')
+        f.write(f"\n{ee}")
+    elif e=='2':
+      eee=input('which todo> ')
+      deleteline(eee)
