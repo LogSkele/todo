56f6ab04fcbe33d6df723341f92c1d0efe06bef3
LogSkele <keystove@gmail.com>
2022-06-09 16:37:03 -0700

    kinda cheating but who cares at this point

--- todo.py
+++ todo.py
@@ -2,15 +2,11 @@ try:f=open("todo.txt", "x")
 except:pass
 while True:
   with open("todo.txt", "rt") as f:
-    print(f.read())
-    print('\n1.create\n2.delete')
-    e=input('> ')
+    print(f.read());print('\n1.create\n2.delete');e=input('> ')
     if e=='1':
       with open("todo.txt", "at") as f:
-        ee=input('content> ')
-        f.write(f"\n{ee}")
+        ee=input('content> ');f.write(f"\n{ee}")
     elif e=='2':
       eee=input('which todo> ')
-      with open("todo.txt", "rt") as f:
-        output = [line for line in f if eee != line.strip()]
+      with open("todo.txt", "rt") as f:output = [line for line in f if eee != line.strip()]
       with open("todo.txt", "wt") as f:f.writelines(output)
